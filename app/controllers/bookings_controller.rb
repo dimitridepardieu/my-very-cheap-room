ecd34eb23daa5a1a0951fc4a6a709e03ebdcb020
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-23 00:13:20 +0100

    Implement create method for bookings and add simple form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.room_id = params[:room_id]
+
+    if @booking.save
+      redirect_to room_path(@booking.room)
+    else
+      render room_path(@booking.room), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_at, :ends_at)
+  end
 end
