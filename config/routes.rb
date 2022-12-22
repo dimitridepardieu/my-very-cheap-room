dfe8007828234e74a4dc5376a2dc27bc159b6d6a
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-22 21:14:37 +0100

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :rooms, only: [index, show] do
+    resources :bookings, only: [create]
+  end
 end
