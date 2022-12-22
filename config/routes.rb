488db5e191c0abcf477c8828eda9b622c3f3d43b
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-22 21:16:45 +0100

    Generate controller for rooms

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :rooms, only: [index, show] do
-    resources :bookings, only: [create]
+  resources :rooms, only: [:index, :show] do
+    resources :bookings, only: [:create]
   end
 end
